cdcb52d3c25ed6dc7064f86666f62594e0997d21
lezca21 <slezcano2103@gmail.com>
2025-02-27 10:31:31 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from PIL import Image 
+
+
+st.title("Hello word")
+
+st.header("En este espacio comienzo a desarrollar mis apps de interfaces multimodales")
+st.write("Facilmente puedo hacer backed y fortend")
+image = Image.open("images.jpeg")
+
+st.image(image, caption="Ay mi madre el bicho")
+
+texto = st.text_input("El mejor jugador de la historia"," Un crack goles de cabeza, pierna derecha, pierna izquierda, alto, veloz, fuerte")
+st.write("Esto es el fucking futbol",texto)
+
+st.subheader("Ahora hay dos columnas")
+
+col1,col2 = st.columns(2)
+
+with col1:
+  st.subheader("primera columna")
+  st.write("Todo es mejor si programando estoy")
+  respecto = st.checkbox("Estoy de acuerdo")
+  if respecto:
+    st.write("Buenisima")
+
+with col2:
+  st.subheader("Segunda columna")
+  modo = st.radio("Tu que prefieres backed o fronted",("backed,fronted,ninguna"))
+  if modo == backend
